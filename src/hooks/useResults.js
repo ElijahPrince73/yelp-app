89d6da976f5d9879efedfc7b7689b25a5071703d
Elijah Prince <elijahprince@Elijahs-MacBook-Pro.local>
2020-03-09 19:37:04 -0700

    Split hooks into own file

---
+++ src/hooks/useResults.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react'
+import yelp from '../api/yelp'
+
+export default () => {
+  const [results, setResults] = useState([])
+  const [errorMessage, setErrorMessage] = useState('')
+
+  const searchApi = async (searchTerm) => {
+    try {
+      const results = await yelp.get('/search', {
+      params: {
+        limit: 50,
+        searchTerm,
+        location: 'las vegas'
+      }
+    })
+    setResults(res.data.businesses)
+    } catch (err) {
+      setErrorMessage('Something went wrong')
+    }
+  }
+
+  useEffect(() => {
+    searchApi('pasta')
+  }, [])
+
+  return [searchApi, results, errorMessage]
+}
