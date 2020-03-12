ba053093db0177faaec5c7f10e33e5b87220738c
Elijah Prince <elijahprince@Elijahs-MacBook-Pro.local>
2020-03-11 21:47:59 -0700

    making request to get a single result/resturant

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import yelp from '../api/yelp'
 
 const ResultsShowScreen = ({ route }) => {
+  const [result, setResult] = useState(null)
   const { id } = route.params
+
+  const getResult = async (id) => {
+    const res = await yelp.get(`/${id}`)
+    setResult(res.data)
+  }
+
+  useEffect(() => {
+    getResult(id)
+  }, [])
 
   return (
     <View>
