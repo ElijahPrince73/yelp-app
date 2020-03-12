fcfa87222236013d0788cfca0093d9bdef951a29
Elijah Prince <elijahprince@Elijahs-MacBook-Pro.local>
2020-03-11 21:33:58 -0700

    removed deeply nested function and used hook to navigate

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
 import ResultsDeatail from './ResultsDetail'
-const ResultsList = ({ title, results, navigation }) => {
+
+const ResultsList = ({ title, results }) => {
+  const navigation = useNavigation();
+  
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{title}</Text>
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -4,7 +4,7 @@ import SearchBar from '../components/SearchBar'
 import useResults from '../hooks/useResults'
 import ResultsList from '../components/ResultsList'
 
-const SearchScreen = ({ navigation }) => {
+const SearchScreen = () => {
     const [term, setTerm] = useState('')
     const [searchApi, results, errorMessage] = useResults()
 
@@ -23,18 +23,15 @@ const SearchScreen = ({ navigation }) => {
         <ScrollView>
           <ResultsList 
             results={filterResultsByPrice("$")} 
-            title="Cost Effective" 
-            navigation={navigation}
+            title="Cost Effective"
           />
           <ResultsList 
             results={filterResultsByPrice("$$")} 
-            title="Bit Pricier" 
-            navigation={navigation}
+            title="Bit Pricier"
           />
           <ResultsList 
             results={filterResultsByPrice("$$$")} 
-            title="Big Spender" 
-            navigation={navigation}
+            title="Big Spender"
           />
         </ScrollView>
       </>
